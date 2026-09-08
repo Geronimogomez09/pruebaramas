dd053224a0bb88702514f2b57bbd581d37f92f8c
AnaPaulaVera <anapaula.vera.epet20@gmail.com>
2026-09-08 15:08:39 -0300

    h

---
+++ src/componentes/footer.jsx
@@ -0,0 +1,14 @@
+// src/components/hero.jsx
+function footer() {
+  return (
+    <section>
+    <div>
+        <h1>holalolxd</h1>
+        <p>ana</p>
+        <a href="#">Comenzar</a>
+    </div>
+</section>
+  );
+}
+
+export default footer;
